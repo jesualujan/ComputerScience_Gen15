0cd1365f7a2fe9a20abbcc913dce79ccdd22ec8c
Jesua Luján <code.masterqro@gmail.com>
2022-04-28 20:00:19 -0500

    Creamos nuestra subclase Pelicula

---
+++ Herencia03/Pelicula.js
@@ -0,0 +1,20 @@
+//* PELICULA ES MI SUBCLASE
+import {Largometraje} from './Largometraje'
+
+class Pelicula extends Largometraje {
+    constructor(titulo,duracion,genero){
+         //? Aquí indicamos que atributos de mi clase padre va a heredar
+        super(titulo,duracion)
+        this.genero=genero
+
+    }
+    //METODO
+      getGender(){
+        return this.genero
+    }
+
+}
+
+//EXPORTAMOS NUESTRA PLANTILLA (CÓDIGO)
+//USAMOS LA FORMA DEFINIDA POR ECMASCRIPT6
+export {Pelicula}
