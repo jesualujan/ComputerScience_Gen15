ac51b65e943705dff0d9f385df6c781d6a17eae3
rene90 <rob_rene199027@hotmail.com>
2022-05-03 19:56:10 -0600

    Pilas 2da parte

--- Pilas05/main.js
+++ Pilas05/main.js
@@ -43,8 +43,64 @@ console.log(pilaAlumnos)
 console.log(pilaAlumnos.apilar("Edwin"))
 console.log(pilaAlumnos.apilar("Paulina"))
 console.log(pilaAlumnos.apilar("Rosaisela"))
+
 console.log(pilaAlumnos)
 
+console.log(pilaAlumnos.borrar())
+console.log(pilaAlumnos)
+console.log(pilaAlumnos.longitud())
+console.log(pilaAlumnos.apilar("Jonathan"))
+console.log(pilaAlumnos.peek())
+
+console.log(pilaAlumnos.estaVacia())
+console.log(pilaAlumnos.apilar("Rene"))
+console.log(pilaAlumnos.apilar("Jesua"))
+console.log(pilaAlumnos.apilar("Iris"))
+console.log(pilaAlumnos)
+//BORRAR TODOS LOS ALUMNOS
+let numeroAlumnos = pilaAlumnos.contador
+for(let i =0; i<numeroAlumnos;i++){
+    pilaAlumnos.borrar()
+}
+
+console.log(pilaAlumnos.estaVacia())
+console.log(pilaAlumnos)
+var arrayEstudiantesAutomatico = ["Rene","Jesua","Iris","Edwin","Mario","Roberto"]
+
+const pilaNombres = new Pila()
+for (let i =0;i< arrayEstudiantesAutomatico.length; i++ ){
+    pilaNombres.apilar(arrayEstudiantesAutomatico[i])
+
+
+}
+console.log(pilaNombres)
+//FUNCION que borra los elementos necesarios para regresar el numero de elementos dado
+// FORMA 1 con RECURSIVIDAD
+const miFuncion = (pila,numero)=>{
+    if(pila.longitud()< numero){
+        return "Elige un numero menor"
+    }
+    if(pila.longitud()!== numero){
+        pila.borrar()
+        return miFuncion(pila,numero)
+    }
+    return pila
+
+}
+// FORMA 2 ciclo FOR
+const miFuncionFor = (pila,numero)=>{
+    let long = pila.longitud()
+    let borrar = long-numero
+    for (let i=0;i< borrar; i++){
+        pila.borrar()
+    }
+    return pila
+
+
+
+}
+
+console.log(miFuncionFor(pilaNombres,2))
 
 
 
