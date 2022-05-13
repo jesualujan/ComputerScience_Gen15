e184355a8bc761b0a9e71d2e0cd63df31e2ae72d
Jesua Luján <code.masterqro@gmail.com>
2022-05-12 20:33:07 -0500

    Crear nuestras clases, y sus respectivos métodos

---
+++ ArbolBinario10/binary_tree.js
@@ -0,0 +1,28 @@
+//PASOS PARA DESARROLLAR EL CÓDIGO DEL ÁRBOL BINARIO
+
+//* CLASE NODO 
+
+class Node { //? CREAR NUESTROS NODOS -> cierta data 
+    constructor(data,left,right){
+        this.data=data
+        this.left=left   
+        this.right=right
+    }
+}
+
+class BinaryTree { //? VAMOS A CONSTRUIR NUESTRO ÁRBOL BINARIO 
+    constructor(){
+       this.root=null 
+    }
+    //VAMOS A TENER COMO METODOS PRINCIPALES
+    //* agregar -> add: se encarga de agregar un dato
+   //*contiente -> contains: verificar si un nodo existe respecto a un dato 
+
+   add(){
+
+   }
+
+   contains(){
+       
+   }
+}
