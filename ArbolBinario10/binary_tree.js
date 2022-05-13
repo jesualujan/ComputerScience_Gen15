095643d8a34a28cb70893488fcb85bc4fc20fff9
Jesua Luján <code.masterqro@gmail.com>
2022-05-12 21:14:47 -0500

    trabajos nuestros métodos de nuestra clase BinaryTree

--- ArbolBinario10/binary_tree.js
+++ ArbolBinario10/binary_tree.js
@@ -18,11 +18,61 @@ class BinaryTree { //? VAMOS A CONSTRUIR NUESTRO ÁRBOL BINARIO
     //* agregar -> add: se encarga de agregar un dato
    //*contiente -> contains: verificar si un nodo existe respecto a un dato 
 
-   add(){
-
+   add(data){
+        //primero que debemos verificar es si root esta vacía
+        if(this.root === null){
+            //vamos a crear un nuevo nodo (data,null,null)
+            this.root = new Node (data, null , null)
+            return 
+        }
+        //si no está vacia
+        //vamos a crear un nuevo nodo, -> currentNode
+        let currentNode = this.root
+        //agregamos un ciclo mientras cada nodo sea un subarbol
+        while(true){
+            if(data < currentNode.data){
+                //revisar si el nodo de la izquierda está vacío
+                if(currentNode.left !== null){
+                    // mi nodo actual le asigne la posición de la izquierda
+                    currentNode = currentNode.left
+                }else{
+                    currentNode.left = new Node(data, null, null)
+                    return currentNode.left
+                }
+            }else {
+                // si el nodo de la derecha está vacio
+                if(currentNode.right !== null){
+                    //agregamos nuevo nodo
+                    currentNode=currentNode.right
+                }else{
+                    //cambiar nuestro nodo actual por el hijo a la derecha 
+                    currentNode.right= new Node (data,null,null)
+                    return currentNode.right
+                }
+            }
+        }
    }
 
-   contains(){
-       
+   contains(data){
+            //partimos de la raiz
+            let currentNode = this.root
+            // mientras currentNode exista 
+            while(currentNode !== null){
+                //exista data adentro del nodo 
+                if(data === currentNode.data){
+                    return true 
+                }else{
+                    // si mi data es menor es decir currentNode.data
+                    if(data < currentNode.data){
+                        //me asigno a la izquierda
+                        currentNode = currentNode.left
+                    }else{
+                        //si mi data no es menor, es mayor
+                        //asignamos a la derecha 
+                        currentNode = currentNode.right
+                    }
+                }
+            }
+            return false 
    }
 }
